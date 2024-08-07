a0e12bbff829e4681225810ef9d62a2560ca8a3d
random-tiger <71456592+random-tiger@users.noreply.github.com>
2024-08-07 13:40:51 -0700

    Update openai_client.py

--- openai_client.py
+++ openai_client.py
@@ -1,29 +1,58 @@
-import openai
+import os
+import requests
 import streamlit as st
+from openai import OpenAI
 
 class OpenAIClient:
     def __init__(self):
         self.api_key = st.secrets["OPENAI_API_KEY"]
-        if not self.api_key:
-            raise ValueError("API key is not set. Please set the OPENAI_API_KEY in Streamlit secrets.")
-        openai.api_key = self.api_key
-    
-    # Function to generate response based on prompt and model
+        self.client = OpenAI(api_key=self.api_key)
+
+    def transcribe_audio(self, audio_file):
+        transcription = self.client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+        return transcription['text'] if isinstance(transcription, dict) else transcription.text
+
     def generate_response(self, transcription, model, custom_prompt):
-        response = openai.ChatCompletion.create(
+        response = self.client.chat.completions.create(
             model=model,
+            temperature=0,
             messages=[
                 {"role": "system", "content": custom_prompt},
                 {"role": "user", "content": transcription}
-            ],
-            temperature=0,
+            ]
         )
         return response.choices[0].message.content
 
-    # Function to transcribe audio using Whisper
-    def transcribe_audio(self, audio_file):
-        transcription = openai.Audio.transcribe(
-            model="whisper-1", file=audio_file
-        )
-        return transcription['text'] if isinstance(transcription, dict) else transcription.text
+    def transcribe_image(self, base64_image):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+
+        payload = {
+            "model": "gpt-4o-mini",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "What’s in this image?"
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}"
+                            }
+                        }
+                    ]
+                }
+            ],
+            "max_tokens": 300
+        }
+
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+        response.raise_for_status()
+        return response.json()['choices'][0]['message']['content']
+
 
